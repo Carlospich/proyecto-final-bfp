2a38d25cfc388aa63b08c269f398216517aa411b
NachoF1M <luis.forero.molina@gmail.com>
2023-11-23 22:38:05 -0500

    Añadir Signup
    Añade signup utilizando firebase

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,35 +1,61 @@
-import React, { useRef } from "react";
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useAuth } from '../contexts/AuthContext'
+
+
 export default function Signup() {
-    const emailRef = useRef();
-    const passwordRef= useRef();
-    const passwordConfimRef= useRef();
-    return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Registrarme</h2>
-                    <Form>
-                        <Form.Group id="email">
-                            <Form.Label>Correo electrónico</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required />
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Confirma tu contraseña</Form.Label>
-                            <Form.Control type="password" ref={passwordConfimRef} required />
-                        </Form.Group>
-                        <Button className="w-100" type="submit">Registrarme</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="W-100 text-center mt-2">
-                ¿Ya tienes una cuenta? Iniciar Sesión
-            </div>
-        </>
-    )
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfimRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfimRef.current.value) {
+
+            return setError('Las contraseñas no coinciden')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Tu cuenta no pudo ser creada')
+        }
+        setLoading(false)
+    }
+  
+
+return (
+    <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Registrarme</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Correo electrónico</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required />
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required />
+                    </Form.Group>
+                    <Form.Group id="password-confirm">
+                        <Form.Label>Confirma tu contraseña</Form.Label>
+                        <Form.Control type="password" ref={passwordConfimRef} required />
+                    </Form.Group>
+                    <Button disabled={loading} className="w-100 mt-3" type="submit">Registrarme</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <div className="W-100 text-center mt-2">
+            ¿Ya tienes una cuenta? Iniciar Sesión
+        </div>
+    </>
+)
 }
